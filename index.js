e595c1cc90052ae29870a421c924d97d85d30bf9
Imran <imranimon1@gmail.com>
2021-11-08 23:51:58 +0100

    removed locale date string

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
         // GET
         app.get('/appointments',verifyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString("de-DE", {timeZone: 'UTC'});
+            const date = req.query.date;
             let appointments = []
             if (email && date) {
                 const query = { email: email, date: date };
