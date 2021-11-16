0b5f9fd51bd530f3ea0d58a3821540d14d545bd4
Imran <imranimon1@gmail.com>
2021-11-16 23:04:32 +0100

    doctors api added

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const cors = require('cors')
 const admin = require("firebase-admin");
 const port = process.env.PORT || 5000
 require('dotenv').config()
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb');
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const fileUpload = require('express-fileupload')
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+app.use(fileUpload())
 
 //doctors-portal-admin.json
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
@@ -12,9 +20,6 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-// middleware
-app.use(cors())
-app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ug28d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -39,9 +44,10 @@ async function run() {
         const database = client.db("DoctorsPortal");
         const appointmentCollection = database.collection("appointments");
         const userCollection = database.collection("users");
+        const doctorCollection = database.collection("doctors");
 
         // GET
-        app.get('/appointments',verifyToken, async (req, res) => {
+        app.get('/appointments', verifyToken, async (req, res) => {
             const email = req.query.email;
             const date = req.query.date;
             let appointments = []
@@ -54,11 +60,55 @@ async function run() {
             res.send(appointments);
         });
 
+        app.get('/appointments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const appointment = await appointmentCollection.findOne(query);
+            res.json(appointment);
+        })
         app.post('/appointments', async (req, res) => {
             const appointment = req.body;
+            console.log(appointment)
             const result = await appointmentCollection.insertOne(appointment);
             res.json(result);
         });
+
+        app.put('/appointments/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    payment: payment
+                }
+            }
+            const result = await appointmentCollection.updateOne(filter, updateDoc);
+            res.json(result)
+        })
+
+
+        app.get('/doctors', async (req, res) => {
+            const cursor = doctorCollection.find({})
+            const doctors = await cursor.toArray();
+            res.json(doctors)
+        })
+
+        app.post('/doctors', async (req, res) => {
+            const name = req.body.name;
+            const email = req.body.email;
+            const img = req.files.image;
+            const imgData = img.data;
+            const encodedImage = imgData.toString('base64');
+            const imageBuffer = Buffer.from(encodedImage, 'base64');
+            const doctor = {
+                name,
+                email,
+                image: imageBuffer
+            }
+            const result = await doctorCollection.insertOne(doctor);
+            res.json(result)
+        })
+
 
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email;
@@ -106,6 +156,19 @@ async function run() {
 
         });
 
+        app.post('/create-payment-intent', async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = paymentInfo.price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'eur',
+                payment_method_types: ['card']
+            })
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        })
+
         app.get("/db", (req, res) => {
             res.send('Database Connected')
         })
@@ -115,7 +178,6 @@ async function run() {
 
 }
 run().catch(console.dir);
-
 
 
 app.get('/', (req, res) => {
